fb98c7dabb745ecc7d926e955b33a4a194ee2f6b
aalexzhang <102832960+aalexzhang@users.noreply.github.com>
2025-02-22 18:12:41 -0800

    vite frontend setup works

--- Dealicious/backend/server.js
+++ Dealicious/backend/server.js
@@ -14,3 +14,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const path = require("path");
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
